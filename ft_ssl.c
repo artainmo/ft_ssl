d394479d1f0f5733575672cb58825fd56feb7538
artainmo <tainmontarthur@icloud.com>
2023-05-01 20:00:15 +0200

    boilerplate setup

---
+++ ft_ssl.c
@@ -0,0 +1,8 @@
+#include "ft_ssl.h"
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	return 0;
+}
---
+++ ft_ssl.h
@@ -0,0 +1,8 @@
+#ifndef FT_SSL_H
+#define FT_SSL_H
+
+#include "utils/libft/libft.h"
+#include "utils/get_next_line/get_next_line.h"
+#include "utils/ft_printf/printf.h"
+
+#endif
